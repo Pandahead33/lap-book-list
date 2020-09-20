161f1845cdfa2002076991391c223c7c1c62d4fd
Lucas Puskaric <igetspammed1234@gmail.com>
2020-09-20 19:39:53 -0400

    Create bookList.js to load in data
    Might be nice to load JSON from another file

---
+++ bookList.js
@@ -0,0 +1,226 @@
+let books = [
+    {
+        title:
+            "How I Built This: The Unexpected Paths to Success from the World’s Most Inspiring Entrepreneurs",
+        imageUrl:
+            "https://images-na.ssl-images-amazon.com/images/I/51iVH1+s-0L._SX329_BO1,204,203,200_.jpg",
+        link: "https://amzn.to/3iNCnrP",
+        author: "Guy Raz",
+        read: false,
+        summary:
+            "I've listened to many episodes of the How I Built This podcast. It is on NPR hosted by the author, Guy Raz. The podcast is about the founding stories of businesses. I was close to not buying this book since I thought it might be a reiteration of podcast content, but it ended up being a great purchase so far. | The book travels through the \"hero's journey\" of entrpreneurship. While it uses examples from the podcasts, there are a lot of stories and much more depth. The most interesting parts are where Guy Raz injects his own story into how he built the podcast and got to his current position. | Additionally, the book is setup in logical order of how one would establish a business from scratch. This makes it an almost how to guide filled with juicy nuggets.",
+        datePurchased: new Date(2020, 8, 16)
+    },
+    {
+        title:
+            "If You Want To Be Rich, Don't Work For Money (Rich Culture Book 1)",
+        imageUrl: "https://m.media-amazon.com/images/I/413PFPN5ZqL.jpg",
+        link: "https://amzn.to/2Ei14NS",
+        author: "David O.",
+        read: true,
+        summary:
+            "I follow David O. on Medium. This is not another self help book. If you want to get rich, read this. This is the book I've always wanted. | Other self help books psych you up so you can \"follow your dreams\". This book is about how systems works, not only hyping yourself up. It isn't vague advice to get you motivated. It is about finding YOUR path. | You can taste the desire coming from David's writing. And it is infectious. | The book outlines how those with a rich mindset create wealth (NOT money) and how you can inject your own knowledge to do the same.",
+        datePurchased: new Date(2020, 7, 14),
+        dateFinished: new Date(2020, 7, 16)
+    },
+    {
+        title:
+            "The Mom Test: How to talk to customers & learn if your business is a good idea when everyone is lying to you",
+        imageUrl: "https://m.media-amazon.com/images/I/41xXPYLVw1L.jpg",
+        link: "https://amzn.to/2FRVpP2",
+        author: "Rob Fitzpatrick",
+        read: true,
+        summary:
+            'This is all about having good customer conversations to validate problems and product decisions. | A favorite metaphor: | People freak out about asking someone on a date, especially on first meeting. If you see a girl you think is cute in the mall, rather than awkwardly asking for a date just get to know her first. Then you can decide if you get along. | Book quote: "It’s obvious that this is a silly situation. After all, the purpose of a date is to talk to each other and see if you get along. Therefore, for all intents and purposes, you were already on one. And then you messed it up by trying to over-formalise it when you could have just chatted a bit and skipped that whole first date completely." | Skip formalizing to save everyone time, worry, and gets actionable information quicker. | It also has a framework for how to go into conversations to get reliable information. If you ask your mom, are you handsome? Of course, she will say yes. But how do you get to the root of things? How do you get the TRUTH? It is usually by bringing things back to how people act or acted in the past.',
+        datePurchased: new Date(2020, 7, 24),
+        dateFinished: new Date(2020, 7, 28)
+    },
+    {
+        title: "White Noise",
+        imageUrl:
+            "https://images-na.ssl-images-amazon.com/images/I/51YX1Pz-foL._SX323_BO1,204,203,200_.jpg",
+        link: "https://amzn.to/360itGa",
+        author: "Don DeLillo",
+        read: true,
+        summary:
+            "Pretty highbrow. | A few quotes I really enjoyed, and the overall story is memorable. Thought provoking and made me laugh at times. I read this mostly because The Airborne Toxic Event band is named after the book. I use to read a lot of this style. | I recommend giving it a shot, but I can't say the energy of the book was distributed continuously. There were times it felt like it was dragging a bit with the mundane- which was probably the point but didn't make it more fun to read.",
+        datePurchased: new Date(2020, 6, 3),
+        dateFinished: new Date(2020, 8, 8)
+    },
+    {
+        title: "21 Lessons for the 21st Century",
+        imageUrl: "https://m.media-amazon.com/images/I/41+uVWlucWL.jpg",
+        link: "https://amzn.to/2RKnomE",
+        author: "Yuval Noah Harari",
+        read: false,
+        summary:
+            "I kept avoiding buying this book, but it went on sale. The hook is interesting but the reviews made it seem lackluster. It kept coming up against and again so I decided to give it a shot. I haven't gotten to deep into it yet, but have been enjoying it thus far.",
+        datePurchased: new Date(2020, 4, 24)
+    },
+    {
+        title: "The Gift: Creativity and the Artist in the Modern World",
+        imageUrl:
+            "https://images-na.ssl-images-amazon.com/images/I/41kbni4cSTL._SX317_BO1,204,203,200_.jpg",
+        link: "https://amzn.to/2RQ0x8V",
+        author: "Lewis Hyde",
+        read: false,
+        summary:
+            "I have many artist friends. I don't 100% recall how I found this book, but I believe another book referred to it. It may have even been Hollywood Park. It has influenced many artists and creatives so I thought I should try to understand it. | It really is about the nature of gifts and argues against the transactional ways when it comes to art. Art only works when it is not treated as a commodity. It starts by tracing the culture of gift giving. It is quite an academic text but it isn't boring. Though a lot of the points seem a bit haphazard- overall it is cohesive. | The book makes complete sense but occasionally makes sweeping views on Western economics and how they oppose art. I'm not sure this is true. I do like the concept of the \"gift economy\" and do believe in the basic premise. I don't see why those two ideas couldn't exist (and the author even provides examples where it does). One point is that gifts cannot be circular or they become transactional. I think Americans really do this and it is silly. I've definitely done this. In a sense, a gift is only a gift when there is no expectation of return or there is a third party involved. For instance, this list is a gift only once I share it to other people.",
+        datePurchased: new Date(2020, 4, 20)
+    },
+    {
+        title: "Hollywood Park: A Memoir",
+        imageUrl:
+            "https://images-na.ssl-images-amazon.com/images/I/41pA8mIXOZL._SX327_BO1,204,203,200_.jpg",
+        link: "https://amzn.to/3j1Rz4N",
+        author: "Mikel Jollett",
+        read: true,
+        summary:
+            "A wild ride. I'm not ashamed to admit there were tears at times. As in multiple times. I finished this a few days after the release date but forgot to post it here. | If you don't care for indie music or know what The Airborne Toxic Event is- read this anyway. This is the story of someone who lived a life most don't even realize other's go through. | The writing style is incredible, the pacing is good- and the story is uniquely Mikel's. No one else could have made this because no one has lived this experience. Even those who were stuck in the situation with him undoubtedly did not end up on a similar path. | I always loved The Airborne Toxic Event (I listen to Songs of God and Whiskey on repeat to this day. Probably my favorite album of all time.), but this book made me love Mikel and his story. It gives context and deeper meaning to their entire repertoire of songs. | We can't easily imagine growing up in a situation outside our own- let alone in a cult! There were so many times in this book that blew my mind. | The cult thing might be the hook- but that is just the beginning. Highly recommended.",
+        datePurchased: new Date(2020, 4, 13),
+        dateFinished: new Date(2020, 4, 20)
+    },
+    {
+        title:
+            "High Performance Habits: How Extraordinary People Become That Way",
+        imageUrl: "https://m.media-amazon.com/images/I/41X5ynWthUL.jpg",
+        link: "https://amzn.to/3hNBG0h",
+        author: "Brendon Burchard",
+        read: false,
+        summary:
+            'My friend Joonie recommended this book to me. He had read "Company of One" by Paul Jarvis that I recommended him and we had a phone call to discuss it. The next call we are going to discuss this and he is reading David O\'s book from this list | The book so far is very good. It is a bit "buy things on my website" and a little more FOLLOW THIS EVERYDAY OR YOU WON\'T SUCCEEDED. But the principles are solid and it looks like they went through a lot of data gathering to figure out why those principles work. It reminds me of this talk I watched recently about retiring early. People simply focus on the wrong things. | That video also showed me things I already knew, but was still a good watch. If interested, look up Early Reitrement in One Lesson (or How I Retired at 30) by Mr. Money Mustache. | If you are off the track, this is a good book to get back onto the path. If you feel stagnate, it is good. People lose sight and don\'t have systems in place. This is definitely targeted more towards people who have already achieved "success"."',
+        datePurchased: new Date(2020, 8, 5)
+    },
+    {
+        title: "Ninja Selling: Subtle Skills. Big Results.",
+        imageUrl:
+            "https://images-na.ssl-images-amazon.com/images/I/41JdzkB+VBL._SX331_BO1,204,203,200_.jpg",
+        link: "https://amzn.to/2FDWz13",
+        author: "Larry Kendall",
+        summary:
+            "Was looking for a book on marketing in general, ended up buying this sales book instead. Luckily, this book is about the intersection of that, it ended up being a good read so far. Rather than traditional sales, the core philsophies stem from other disciplines. A lot of this seems common sense or obvious (but I have read many other books), but it does differ from other sales books. Couldn't find the actual date I got this one so I set it to before anything else.",
+        datePurchased: new Date(2020, 0, 0)
+    }
+];
+
+let readBooks = [];
+let unreadBooks = [];
+
+function comparator(a, b, field) {
+    if (a[field] > b[field]) {
+        return -1;
+    } else if (a[field] < b[field]) {
+        return 1;
+    }
+    return 0;
+}
+
+books = books.sort(function (a, b) {
+    const DATE_FINISHED_FIELD = "dateFinished";
+    const DATE_PURCHASED_FIELD = "datePurchased";
+
+    if (a.read && b.read) {
+        return comparator(a, b, DATE_FINISHED_FIELD);
+    }
+
+    if (a.read && !b.read) {
+        return -1;
+    }
+
+    if (!a.read && b.read) {
+        return 1;
+    }
+
+    return comparator(a, b, DATE_PURCHASED_FIELD);
+});
+
+books.forEach((book) => {
+    if (book.read) {
+        readBooks.push(book);
+    } else {
+        unreadBooks.push(book);
+    }
+});
+
+books = [...readBooks, ...unreadBooks];
+
+for (let book in books) {
+    let bookCardNode = document.createElement("DIV");
+    bookCardNode.classList.add("cell");
+
+    let titleNode = document.createElement("P");
+    titleNode.appendChild(document.createTextNode(books[book].title));
+
+    let imageNode = document.createElement("IMG");
+    imageNode.setAttribute("src", books[book].imageUrl);
+
+    let linkContainerNode = document.createElement("P");
+    let linkNode = document.createElement("A");
+    linkNode.setAttribute("href", books[book].link);
+    linkNode.setAttribute("target", "_blank");
+    linkNode.appendChild(document.createTextNode("Amazon Link"));
+    linkContainerNode.appendChild(linkNode);
+
+    bookCardNode.appendChild(titleNode);
+    bookCardNode.appendChild(imageNode);
+    bookCardNode.appendChild(linkContainerNode);
+
+    let authorCardNode = document.createElement("DIV");
+    authorCardNode.classList.add("cell");
+    let authorParagraphNode = document.createElement("P");
+    let authorNode = document.createTextNode(books[book].author);
+    authorParagraphNode.appendChild(authorNode);
+    authorCardNode.appendChild(authorParagraphNode);
+
+    let finishedReadingNode = document.createElement("DIV");
+    finishedReadingNode.classList.add("cell");
+    let readParagraphNode = document.createElement("P");
+    let readNode = document.createTextNode(books[book].read ? "✔️" : "❌");
+    readParagraphNode.appendChild(readNode);
+    finishedReadingNode.appendChild(readParagraphNode);
+
+    let summaryNode = document.createElement("DIV");
+    summaryNode.classList.add("cell");
+
+    let sentences = books[book].summary.split("|");
+
+    if (sentences && sentences.length > 0) {
+        for (let sentence in sentences) {
+            if (sentences[sentence]) {
+                let paragraphNode = document.createElement("P");
+                paragraphNode.appendChild(
+                    document.createTextNode(sentences[sentence])
+                );
+                summaryNode.appendChild(paragraphNode);
+            }
+        }
+    }
+
+    let datePurchasedContainerNode = document.createElement("DIV");
+    datePurchasedContainerNode.classList.add("cell");
+    let datePurchasedParagraphNode = document.createElement("P");
+    let datePurchasedNode = document.createTextNode(
+        books[book].datePurchased.toDateString()
+    );
+    datePurchasedParagraphNode.appendChild(datePurchasedNode);
+    datePurchasedContainerNode.appendChild(datePurchasedParagraphNode);
+
+    let dateFinishedContainerNode = document.createElement("DIV");
+    dateFinishedContainerNode.classList.add("cell");
+    let dateFinishedParagraphNode = document.createElement("P");
+    let dateFinishedNode = document.createTextNode(
+        books[book].dateFinished
+            ? books[book].dateFinished.toDateString()
+            : "❌"
+    );
+    dateFinishedParagraphNode.appendChild(dateFinishedNode);
+    dateFinishedContainerNode.appendChild(dateFinishedParagraphNode);
+
+    let bodyNode = document.getElementsByTagName("body")[0];
+    bodyNode.appendChild(bookCardNode);
+    bodyNode.appendChild(authorCardNode);
+    bodyNode.appendChild(finishedReadingNode);
+    bodyNode.appendChild(summaryNode);
+    bodyNode.appendChild(datePurchasedContainerNode);
+    bodyNode.appendChild(dateFinishedContainerNode);
+}
